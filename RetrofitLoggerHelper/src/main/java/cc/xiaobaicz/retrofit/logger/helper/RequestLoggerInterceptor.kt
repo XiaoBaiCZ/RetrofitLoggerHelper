6435d729d84d2c9bb15b5b96b49a7c4584e76d15
xiaobai <1063779828@qq.com>
2020-12-23 16:14:33 +0800

    新增自定义格式化输出

--- RetrofitLoggerHelper/src/main/java/cc/xiaobaicz/retrofit/logger/helper/RequestLoggerInterceptor.kt
+++ RetrofitLoggerHelper/src/main/java/cc/xiaobaicz/retrofit/logger/helper/RequestLoggerInterceptor.kt
@@ -1,6 +1,7 @@
 package cc.xiaobaicz.retrofit.logger.helper
 
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import okio.Buffer
 import java.io.OutputStream
@@ -18,7 +19,7 @@ import java.util.*
  * @param oStream 输出流
  * @param isShowHeader 是否打印Http头数据
  */
-class RequestLoggerInterceptor private constructor(val oStream: OutputStream, val isShowHeader: Boolean) : Interceptor {
+class RequestLoggerInterceptor private constructor(val oStream: OutputStream, val isShowHeader: Boolean, val format: Format) : Interceptor {
 
     companion object {
         /**
@@ -30,35 +31,7 @@ class RequestLoggerInterceptor private constructor(val oStream: OutputStream, va
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val req = chain.request()
-        val sb = StringBuilder()
-        sb.append("====================Request==========================").appendln()
-        //时间
-        sb.append(SimpleDateFormat.getInstance().format(Date())).appendln()
-        //请求信息
-        sb.append("${req.method()} ${req.url()}").appendln()
-        if (isShowHeader) {
-            //头部信息
-            sb.append("-----------------------------------------------------").appendln()
-            sb.append(req.headers().toString()).appendln()
-            sb.append("-----------------------------------------------------").appendln()
-        } else {
-            sb.appendln()
-        }
-        oStream.write(sb.toString().toByteArray())
-        req.body()?.apply {
-            //body信息
-            Buffer().use { buff ->
-                this.writeTo(buff)
-                buff.copyTo(oStream)
-            }
-        }
-        sb.clear()
-        sb.appendln().append("=====================================================").appendln().appendln()
-        //结束
-        oStream.write(sb.toString().toByteArray())
-        oStream.flush()
-        return chain.proceed(req)
+        return chain.proceed(format.format(this, oStream, chain.request()))
     }
 
     /**
@@ -74,6 +47,11 @@ class RequestLoggerInterceptor private constructor(val oStream: OutputStream, va
          * 显示头信息？
          */
         private var isShowHeader: Boolean = false
+
+        /**
+         * 格式化到输出流
+         */
+        private var format: Format = DefFormat()
 
         /**
          * 设置输出流
@@ -92,14 +70,70 @@ class RequestLoggerInterceptor private constructor(val oStream: OutputStream, va
         }
 
         /**
+         * 设置输出格式化
+         */
+        fun setFormat(format: Format): Builder {
+            this.format = format
+            return this
+        }
+
+        /**
          * 构建一个拦截器
          * @param out 是否输出
          */
         fun build(out: Boolean = true): Interceptor = if (out) {
-            RequestLoggerInterceptor(oStream, isShowHeader)
+            RequestLoggerInterceptor(oStream, isShowHeader, format)
         } else {
             EmptyInterceptor()
         }
     }
 
+    /**
+     * 格式化输出
+     */
+    fun interface Format {
+        /**
+         * @param interceptor 拦截器信息
+         * @param out 输出流
+         * @param req 请求体
+         * @return 请求体 -> 可原样返回
+         */
+        fun format(interceptor: RequestLoggerInterceptor, out: OutputStream, req: Request): Request
+    }
+
+    /**
+     * 默认格式化实现
+     */
+    private class DefFormat : Format {
+        override fun format(interceptor: RequestLoggerInterceptor, out: OutputStream, req: Request): Request {
+            val sb = StringBuilder()
+            sb.append("====================Request==========================").appendln()
+            //时间
+            sb.append(SimpleDateFormat.getInstance().format(Date())).appendln()
+            //请求信息
+            sb.append("${req.method()} ${req.url()}").appendln()
+            if (interceptor.isShowHeader) {
+                //头部信息
+                sb.append("-----------------------------------------------------").appendln()
+                sb.append(req.headers().toString()).appendln()
+                sb.append("-----------------------------------------------------").appendln()
+            } else {
+                sb.appendln()
+            }
+            out.write(sb.toString().toByteArray())
+            req.body()?.apply {
+                //body信息
+                Buffer().use { buff ->
+                    this.writeTo(buff)
+                    buff.copyTo(out)
+                }
+            }
+            sb.clear()
+            sb.appendln().append("=========================End=========================").appendln().appendln()
+            //结束
+            out.write(sb.toString().toByteArray())
+            out.flush()
+            return req
+        }
+    }
 }
--- RetrofitLoggerHelper/src/main/java/cc/xiaobaicz/retrofit/logger/helper/ResponseLoggerInterceptor.kt
+++ RetrofitLoggerHelper/src/main/java/cc/xiaobaicz/retrofit/logger/helper/ResponseLoggerInterceptor.kt
@@ -3,7 +3,6 @@ package cc.xiaobaicz.retrofit.logger.helper
 import okhttp3.Interceptor
 import okhttp3.Response
 import okhttp3.ResponseBody
-import okio.Buffer
 import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
@@ -15,7 +14,7 @@ import java.util.*
  * @param oStream 输出流
  * @param isShowHeader 是否打印Http头数据
  */
-class ResponseLoggerInterceptor private constructor(val oStream: OutputStream, val isShowHeader: Boolean) : Interceptor {
+class ResponseLoggerInterceptor private constructor(val oStream: OutputStream, val isShowHeader: Boolean, val format: Format) : Interceptor {
     companion object {
         /**
          * 创建一个默认的Logger拦截器，输出到控制台，不打印头信息
@@ -25,38 +24,7 @@ class ResponseLoggerInterceptor private constructor(val oStream: OutputStream, v
         fun create(out: Boolean = true): Interceptor = Builder().build(out)
     }
     override fun intercept(chain: Interceptor.Chain): Response {
-        var rsp = chain.proceed(chain.request())
-        val sb = StringBuilder()
-        sb.append("====================Response=========================").appendln()
-        //时间
-        sb.append(SimpleDateFormat.getInstance().format(Date())).appendln()
-        //请求信息
-        sb.append(rsp.request().url()).appendln()
-        //响应信息
-        sb.append("${rsp.protocol()} ${rsp.code()} ${rsp.message()}").appendln()
-        if (isShowHeader) {
-            //头部信息
-            sb.append("-----------------------------------------------------").appendln()
-            sb.append(rsp.headers().toString()).appendln()
-            sb.append("-----------------------------------------------------").appendln()
-        } else {
-            sb.appendln()
-        }
-        oStream.write(sb.toString().toByteArray())
-        rsp.body()?.apply {
-            //body信息
-            val content = this.bytes()
-            rsp = rsp.newBuilder()
-                    .body(ResponseBody.create(this.contentType(), content))
-                    .build()
-            oStream.write(content)
-        }
-        sb.clear()
-        sb.appendln().append("=====================================================").appendln().appendln()
-        //结束
-        oStream.write(sb.toString().toByteArray())
-        oStream.flush()
-        return rsp
+        return format.format(this, oStream, chain.proceed(chain.request()))
     }
 
     /**
@@ -72,6 +40,11 @@ class ResponseLoggerInterceptor private constructor(val oStream: OutputStream, v
          * 显示头信息？
          */
         private var isShowHeader: Boolean = false
+
+        /**
+         * 格式化到输出流
+         */
+        private var format: Format = DefFormat()
 
         /**
          * 设置输出流
@@ -90,13 +63,74 @@ class ResponseLoggerInterceptor private constructor(val oStream: OutputStream, v
         }
 
         /**
+         * 设置输出格式化
+         */
+        fun setFormat(format: Format): Builder {
+            this.format = format
+            return this
+        }
+
+        /**
          * 构建一个拦截器
          * @param out 是否输出
          */
         fun build(out: Boolean = true): Interceptor = if (out) {
-            ResponseLoggerInterceptor(oStream, isShowHeader)
+            ResponseLoggerInterceptor(oStream, isShowHeader, format)
         } else {
             EmptyInterceptor()
+        }
+    }
+
+    /**
+     * 格式化输出
+     */
+    fun interface Format {
+        /**
+         * @param interceptor 拦截器信息
+         * @param out 输出流
+         * @param rsp 响应体
+         * @return 响应体 -> 可原样返回
+         */
+        fun format(interceptor: ResponseLoggerInterceptor, out: OutputStream, rsp: Response): Response
+    }
+
+    /**
+     * 默认格式化实现
+     */
+    private class DefFormat : Format {
+        override fun format(interceptor: ResponseLoggerInterceptor, out: OutputStream, response: Response): Response {
+            var rsp = response
+            val sb = StringBuilder()
+            sb.append("====================Response=========================").appendln()
+            //时间
+            sb.append(SimpleDateFormat.getInstance().format(Date())).appendln()
+            //请求信息
+            sb.append(rsp.request().url()).appendln()
+            //响应信息
+            sb.append("${rsp.protocol()} ${rsp.code()} ${rsp.message()}").appendln()
+            if (interceptor.isShowHeader) {
+                //头部信息
+                sb.append("-----------------------------------------------------").appendln()
+                sb.append(rsp.headers().toString()).appendln()
+                sb.append("-----------------------------------------------------").appendln()
+            } else {
+                sb.appendln()
+            }
+            out.write(sb.toString().toByteArray())
+            rsp.body()?.apply {
+                //body信息
+                val content = this.bytes()
+                rsp = rsp.newBuilder()
+                        .body(ResponseBody.create(this.contentType(), content))
+                        .build()
+                out.write(content)
+            }
+            sb.clear()
+            sb.appendln().append("=========================End=========================").appendln().appendln()
+            //结束
+            out.write(sb.toString().toByteArray())
+            out.flush()
+            return rsp
         }
     }
 
